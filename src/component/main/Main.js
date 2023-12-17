a858cc5a838ff0af7464b52cef696eae03b10943
Louis Blonde <loul0uboypy@gmail.com>
2023-12-17 17:20:47 +0100

    try to listen audio

--- src/component/main/Main.js
+++ src/component/main/Main.js
@@ -1,7 +1,26 @@
 import React from "react";
+import pack from "../../musicPack.json";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <div className="musicContainer">
+        {pack.map((element, index) => {
+          return (
+            <div className="individualMusic" key={index}>
+              <div>{element.song}</div>
+              <div>
+                <audio controls>
+                  <source src={element.link} type="audio/wav" />
+                </audio>
+              </div>
+              <div>Pushed by : {element.by}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Main;
